7ac894ee248c94bd9e8ad5c429926ca8af8af15f
baptistelambert <bapthf2@gmail.com>
2016-02-22 15:18:30 +0100

    Added radar on missions view

--- js/app/views/missions.js
+++ js/app/views/missions.js
@@ -9,6 +9,7 @@ var Missions = function(){
 Missions.prototype = Object.create(View.prototype);
 
 Missions.prototype.bind = function() {
+    this.radar();
 };
 
 Missions.prototype.animateIn = function() {
@@ -36,5 +37,45 @@ Missions.prototype.animateOut = function() {
     });
 
 };
+
+Missions.prototype.radar = function() {
+    var time = 5;
+    var peopleCount = 20;
+    var peoples = [];
+
+    for (i = 0; i < peopleCount; i++) {
+        peoples.push({
+            distance : Math.floor((Math.random() * 140) + 1),
+            angle    : Math.floor((Math.random() * 360) + 1)
+        });
+    }
+
+    (function radar(){
+
+        var radius = 150;
+        for (i = 0; i < peoples.length; i++) {
+            var disX = (90 < peoples[i].angle + 90 < 270) ? radius - peoples[i].distance : radius,
+                disY = (180 < peoples[i].angle + 90 < 360) ? radius - peoples[i].distance : radius,
+                angleNew = (peoples[i].angle + 90) * Math.PI / 180,
+                getDegX = disX + peoples[i].distance - Math.round(peoples[i].distance * Math.cos(angleNew)),
+                getDegY = disY + peoples[i].distance - Math.round(peoples[i].distance * Math.sin(angleNew)),
+                delay = time / 360 * peoples[i].angle;
+
+            $('#guides').append($('<span>')
+                .addClass('dot')
+                .css({
+                    left : getDegX,
+                    top  : getDegY,
+                    '-webkit-animation-delay' : delay + 's',
+                    'animation-delay' : delay + 's'
+                })
+                .attr({
+                    'data-atDeg' : peoples[i].angle
+                }));
+            $("#radar").addClass('animated');
+        }
+
+    })();
+}
 
 
